4a0cfd448c8258e5ed2d29acaf611fa47a63a747
stefano-marchese-majorana <stefano.marchese.majorana@gmail.com>
2022-03-25 05:37:17 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleAppBinary20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // visualizzazione dei messaggi di inzio programma
+
+            // input del scelta da parte dell'utente
+
+            // esecuzione codifica binaria
+
+            // visualizzazione del risultato ottenuto
+
+            // visualizzazione dei messaggi di fine programma
+        }
+    }
+}
